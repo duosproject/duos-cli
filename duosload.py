985d2601e3a4818bfd173bb3e601ada974f93c51
Ainsley McGrath <mcgrath.ainsley@gmail.com>
2018-11-12 17:51:26 -0600

    read csv
    Signed-off-by: Ainsley McGrath <mcgrath.ainsley@gmail.com>

--- duosload.py
+++ duosload.py
@@ -14,91 +14,106 @@ from dotenv import load_dotenv
 import os
 import click
 from click import echo
+import pandas as pd
 
-# config-ing
-load_dotenv()
-db = {
-    "drivername": "postgres",
-    "username": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT"),
-    "database": os.getenv("DB_NAME"),
+RECORD_HANDLING_MAP = {
+    "articles": {
+        "columns": {"duos_label": str, "title": str, "author_list": str},
+        "processor": lambda x: x,
+    },
+    "datasets": {"columns": {}, "processor": lambda x: x},
 }
-connstr = URL(**db)
-engine = create_engine(connstr)
-metadata = MetaData(bind=engine)
-metadata.reflect(bind=engine)
 
 
 @click.group()
-def loadutil():
+@click.pass_context
+def duosload(ctx):
     """utilitiy for loading the data for the DUOS research study."""
-    pass
+
+    load_dotenv()
+    connstr = URL(
+        **{
+            "drivername": "postgres",
+            "username": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+            "host": os.getenv("DB_HOST"),
+            "port": os.getenv("DB_PORT"),
+            "database": os.getenv("DB_NAME"),
+        }
+    )
+    engine = create_engine(connstr)
+    metadata = MetaData(bind=engine)
+    metadata.reflect(bind=engine)
+
+    ctx.obj = {"metadata": metadata, "engine": engine}
 
 
-@loadutil.command()
+@duosload.command()
 @click.option(
     "--from-scratch", is_flag=True, help="drop and recreate the entire database"
 )
-def create(from_scratch):
+@click.pass_context
+def create(ctx, from_scratch):
     if from_scratch:
-        echo(f"⬇  dropping every table... all {len(metadata.tables)} of them")
-        metadata.drop_all(engine)
+        echo(
+            f"⬇  dropping every table... all {len(ctx.obj['metadata'].tables)} of them"
+        )
+        ctx.obj["metadata"].drop_all(ctx.obj["engine"])
         echo("🙌  done!")
+        return
 
-    if len(metadata.tables) > 0:
-        echo(f"🚫  {len(metadata.tables)} already exist.")
+    if len(ctx.obj["metadata"].tables) > 0:
+        echo(f"🚫  {len(ctx.obj['metadata'].tables)} already exist.")
         echo("consider destroying first or using '--from-scratch'")
         return
 
     echo("✨  creating tables...")
-    make_all_the_tables(metadata, engine)
+    make_all_the_tables(ctx.obj["metadata"], ctx.obj["engine"])
     echo("🙌  done!")
+    return
 
 
-@loadutil.command()
-# TODO: option for listing tables to drop
-def destroy():
-    echo(f"⬇  dropping every table... all {len(metadata.tables)} of them")
-    metadata.drop_all(engine)
+@duosload.command()
+@click.pass_context
+def destroy(ctx):
+    echo(f"⬇  dropping every table... all {len(ctx.obj['metadata'].tables)} of them")
+    ctx.obj["metadata"].drop_all(ctx.obj["engine"])
     echo("🙌  destroyed!")
 
 
-@loadutil.command(
-    help="""
-    insert data into the database using a csv\n
-    ℹ️  optional if file is named 'articles.csv' 'references.csv'
-    """
-)
-@click.option("--file", help="the file to upload into the db")
-@click.option(
-    "--mode",
-    help="duos data set type: articles or references ",
-    type=click.Choice(["articles", "references"]),
-)
-def upload(file):
-    echo(file)
+@duosload.command(help="insert local csv into the database")
+def upload():
+    # extract absolute path of wherever we are execxuting
+    path = os.path.dirname(os.path.abspath(__file__))
+    csv_names = {fname.split(".")[0] for fname in os.listdir(path) if ".csv" in fname}
+    echo(f"CSVs discovered: {csv_names}")
+    for name in csv_names:
+        echo(name in RECORD_HANDLING_MAP)
+        # if name in RECORD_HANDLING_MAP:
+        iter_parse_csv(name, path)
 
 
-@loadutil.command(help="list basic info about duos db")
-def info():
-    echo(f"💻  There are {len(metadata.tables)} tables in the DUOS database.")
+@duosload.command(help="list basic info about duos db")
+@click.pass_context
+def info(ctx):
+    echo(id)
+    echo(f"💻  There are {len(ctx.obj['metadata'].tables)} tables in the DUOS database.")
 
 
 # no commands past here
 def make_all_the_tables(metadata, engine):
     """wrapper for database definitions. consider it config."""
 
-    article = Table(
+    Table(
         "article",
         metadata,
         # integer pk interpreted as SERIAL by default
         Column("article_id", Integer, primary_key=True),
         Column("article_title", String, nullable=False),
+        Column("duos_label", String, nullable=False),
     )
 
-    author = Table(
+    Table(
         "author",
         metadata,
         Column("author_id", Integer, primary_key=True),
@@ -106,7 +121,7 @@ def make_all_the_tables(metadata, engine):
         Column("email_address", String, nullable=False),
     )
 
-    dataset = Table(
+    Table(
         "dataset",
         metadata,
         Column("dataset_id", Integer, primary_key=True),
@@ -114,7 +129,7 @@ def make_all_the_tables(metadata, engine):
         Column("abbreviation", String),
     )
 
-    writes = Table(
+    Table(
         "writes",
         metadata,
         Column("writes_id", Integer, primary_key=True),
@@ -122,15 +137,16 @@ def make_all_the_tables(metadata, engine):
         Column("author_id", Integer, ForeignKey("author.author_id"), nullable=False),
     )
 
-    reference = Table(
+    Table(
         "reference",
         metadata,
         Column("ref_id", Integer, primary_key=True),
         Column("dataset_id", Integer, ForeignKey("dataset.dataset_id"), nullable=False),
         Column("article_id", Integer, ForeignKey("article.article_id"), nullable=False),
+        Column("ref_hash", String, nullable=False),
     )
 
-    validates = Table(
+    Table(
         "validates",
         metadata,
         Column("validation_id", Integer, primary_key=True),
@@ -145,5 +161,63 @@ def make_all_the_tables(metadata, engine):
     metadata.create_all()
 
 
+def parse_artist(record_iterable):
+    # split author list to rows
+    # de-dupe paper list
+    # zip up into writes -- 1Article:MAuthors
+    print(record_iterable)
+
+
+def normalize(record_iterable, name):
+    for record in record_iterable:
+        parse_artist(record)
+
+
+def iter_parse_csv(name, path):
+    """ingest and validate csv files
+    :yield:    dict(str, str)"""
+
+    df = pd.read_csv(
+        os.path.join(path, f"{name}.csv"),
+        index_col=False,
+        quotechar='"',
+        skipinitialspace=True,
+        converters=RECORD_HANDLING_MAP[name]["columns"],
+    )
+    print("hello")
+    df_cols = set(df.columns)
+    # iterate over columns in df, missing_column will be name of that column if exists
+    missing_column = next(
+        (col for col in RECORD_HANDLING_MAP[name]["columns"] if col not in df_cols),
+        None,
+    )
+
+    if missing_column is not None:
+        raise ValueError(f"Missing or misnamed column: {missing_column} in {name}.csv.")
+
+    if len(RECORD_HANDLING_MAP[name]) != len(df_cols):
+        extraneous_columns = [
+            col for col in df_cols if col not in RECORD_HANDLING_MAP[name]["columns"]
+        ]
+
+        raise ValueError(
+            f"{len(extraneous_columns)} invalid columns provided:\n\t{extraneous_columns} in {name}.csv."
+        )
+
+    for idx, named_tuple_record in enumerate(df.itertuples(index=False)):
+        record = dict(named_tuple_record._asdict())
+        echo("hello?")
+        if any([len(val) == 0 for val in record.values()]):
+            raise ValueError(f"Malformed record at row {idx} of {name}.csv.")
+
+        yield record
+
+
+def upload_csv():
+    """upload normalized duos data to db in a sensible order"""
+    pass
+
+
 if __name__ == "__main__":
-    loadutil()
+    duosload(obj={})  # initialize empty context
+
